f9594312f7255fd99ca74e8c8fa7bc105d2596f8
nmhyrhn <gbs06193@gmail.com>
2026-05-30 18:14:34 +0900

    jdbcTemplate 작성

--- backend/src/main/java/com/minicart/common/JDBCTemplate.java
+++ backend/src/main/java/com/minicart/common/JDBCTemplate.java
@@ -1,4 +1,63 @@
 package com.minicart.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
 public class JDBCTemplate {
+
+    public static Connection getConnection() {
+        Connection con = null;
+        Properties prop = new Properties();
+
+        try (InputStream input = JDBCTemplate.class
+                .getClassLoader()
+                .getResourceAsStream("db.properties")) {
+
+            prop.load(input);
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url, prop);
+
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+    public static void close(Connection con) {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement stmt) {
+        try {
+            if (stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet rset) {
+        try {
+            if (rset != null && !rset.isClosed()) {
+                rset.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
